2c78a4e7d3933555813c99f8569f4dfdb85062fd
myway00 <junior0614@naver.com>
2022-11-19 12:06:25 +0900

    #2 ADD : 프로젝트 생성 및 설정 파일 추가

---
+++ src/test/java/server/daangn/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package server.daangn.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
